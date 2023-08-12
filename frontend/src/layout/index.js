82abc737a87e6f3b5fd970808868b36b7faffc12
carlos <orellano428@gmail.com>
2023-08-11 19:12:29 -0500

    UI

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -38,6 +38,10 @@ const useStyles = makeStyles((theme) => ({
 
   toolbar: {
     paddingRight: 24, // keep right padding when drawer closed
+    background:
+      "linear-gradient(to right, #0045aa 0%,#4565d0 31%,#7087f7 64%,#7db9e8 100%); ",
+    color: "#FFF",
+    height: "60px",
   },
   toolbarIcon: {
     display: "flex",
@@ -48,6 +52,7 @@ const useStyles = makeStyles((theme) => ({
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
+    height: "60px",
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
